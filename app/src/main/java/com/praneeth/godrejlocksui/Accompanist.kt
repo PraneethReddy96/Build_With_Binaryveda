43172939db215382cf07936dbac968361ca20741
DineshReddyP <praneeth.t96@gmail.com>
2021-09-27 15:05:56 +0530

    final commit

--- app/src/main/java/com/praneeth/godrejlocksui/Accompanist.kt
+++
@@ -1,7 +0,0 @@
-package com.praneeth.godrejlocksui
-
-object Accompanist {
-
-    private const val animationsVersion = "0.18.0"
-    const val animations = "com.google.accompanist:accompanist-navigation-animation:$animationsVersion"
-}
